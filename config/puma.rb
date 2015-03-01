20cfbd3a91afb54a20ee48b01762e24c8b2a8ed3
John Wilger <johnwilger@gmail.com>
2015-02-28 16:43:43 -0800

    Default to running in only one thread

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,11 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 4)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads_count = Integer(ENV['MAX_THREADS'] || 1)
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
-puts ENV['PORT']
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
