62b3ca7f5008fd4eee452629b2e00b0fc0cc4975
John Wilger <johnwilger@gmail.com>
2015-03-03 09:23:15 -0800

    Adds MealsController#new

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,11 @@
 class MealsController < ApplicationController
+  service(:meals) { Meal }
+
+  template_attr :meal
+
+  def new
+    self.meal = meals.new(date: Date.new(params[:year].to_i, params[:month].to_i,
+                                         params[:day].to_i))
+    render
+  end
 end
--- spec/controllers/meals_controller_spec.rb
+++ spec/controllers/meals_controller_spec.rb
@@ -7,5 +7,38 @@ describe MealsController do
         route_to(controller: 'meals', action: 'new', year: '2013', month: '10',
                  day: '19')
     end
+
+    let(:date) { Date.new(2014,11,18) }
+    let(:year) { date.year.to_s }
+    let(:month) { date.month.to_s }
+    let(:day) { date.day.to_s }
+
+    let(:meals_service) { double(:meals_service, new: meal) }
+    let(:meal) { double(:meal) }
+
+    before(:each) do
+      controller.load_services(meals: meals_service)
+      get :new, year: year, month: month, day: day
+    end
+
+    it 'responds with a 200 status' do
+      expect(response.status).to eq 200
+    end
+
+    it 'renders the meals/new template' do
+      expect(response).to render_template 'meals/new'
+    end
+
+    it 'renders as HTML' do
+      expect(response.content_type).to eq 'text/html'
+    end
+
+    it 'builds a new Meal object with the date from the URL params' do
+      expect(meals_service).to have_received(:new).with(date: date)
+    end
+
+    it 'assigns the Meal object to the template' do
+      expect(controller.meal).to eq meal
+    end
   end
 end
